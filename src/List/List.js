f4bb240f2441578cbd7bbcb0c1c3498a3b022c9f
unknown <sebastian.rak9@gmail.com>
2022-04-17 02:35:31 +0200

    ListOfColors basic default variant -done

--- src/List/List.js
+++ src/List/List.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 
 const List = ({basicColors, quantity}) => {
     const [listOfColors, setListOfColors] = useState([]);
+    const [shortcut,setShortcut] = useState([]);
 
     useEffect(()=>{
         const storageColors = JSON.parse(localStorage.getItem('Colors'));
@@ -35,6 +36,14 @@ const List = ({basicColors, quantity}) => {
             return ConvertRGBtoHex(...color);
         })
 
+        const shortCut = sortedHex.map(color=>{
+            if (color[1] === color[2] && color[3] === color[4] && color[5] === color[6]){
+                return ('#' + color[1] + color[3] + color[5]).toUpperCase();
+            }else{
+                return ''
+            }
+        })
+        setShortcut(shortCut);
         setListOfColors(sortedHex);
 
     },[quantity,listOfColors]);
@@ -121,14 +130,35 @@ const List = ({basicColors, quantity}) => {
         <main className='list_holder'>
             <ul className='list'>
                 {listOfColors.map((color, index) => {
+
+                    const padZero = (str, len = 2) => {
+                        let zeros = new Array(len).join('0');
+                        return (zeros + str).slice(-len);
+                    }
+                    let invertColor = color;
+                    if (invertColor.indexOf('#') === 0) {
+                        invertColor = invertColor.slice(1);
+                    }
+                    let red = (255 - parseInt(invertColor.slice(0, 2), 16)).toString(16);
+                    let green = (255 - parseInt(invertColor.slice(2, 4), 16)).toString(16);
+                    let blue = (255 - parseInt(invertColor.slice(4, 6), 16)).toString(16);
+
+                    invertColor = '#' + padZero(red) + padZero(green) + padZero(blue);
+
+                    const inlineStyle = {backgroundColor: `${color}`, borderColor: `${invertColor}`}
+
                     if (basicColors.indexOf(color) < 0) {
                         return (
                             <li key={color} className='list_item'>
-                                <div className={`list_item_picker picker_${index}`} />
+                                <div className={`list_item_picker picker_${index}`} style={inlineStyle}/>
+                                <div className="decorate">
+                                    <div className="right_arrow" style={inlineStyle}/>
+                                    <div className="left_arrow" style={inlineStyle}/>
+                                </div>
                                 <div className='list_item_' >
                                     <span className='list_item__index'>{index + 1}.</span>
                                     <h4 className='list_item__color'>{color}</h4>
-                                    <p className='list_item__shortcut'>shortcut</p>
+                                    <p className='list_item__shortcut'>{shortcut[index]}</p>
                                     <div className='list_item__delete' data-color={color} onClick={e => {handleDelete(e)}}>x</div>
                                 </div>
                             </li>
@@ -136,11 +166,15 @@ const List = ({basicColors, quantity}) => {
                     } else {
                         return (
                             <li key={color} className='list_item'>
-                                <div className={`list_item_picker picker_${index}`} />
+                                <div className={`list_item_picker picker_${index}`} style={inlineStyle}/>
+                                <div className="decorate">
+                                    <div className="right_arrow" style={inlineStyle}/>
+                                    <div className="left_arrow" style={inlineStyle}/>
+                                </div>
                                 <div className='list_item_'>
                                     <span className='list_item__index'>{index + 1}.</span>
                                     <h4 className='list_item__color'>{color}</h4>
-                                    <p className='list_item__shortcut'>shortcut</p>
+                                    <p className='list_item__shortcut'>{shortcut[index]}</p>
                                     <div className='list_item__cover' />
                                 </div>
                             </li>
